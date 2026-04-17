1e0e9950670fe74d43bf3ef8d0fecee90f56382c
isaacandresbaleero-hub <isaacandresbaleero@gmail.com>
2026-04-17 15:20:31 +0300

    Admin dashboard

--- inventory_management/asiatoursagency/admin.py
+++ inventory_management/asiatoursagency/admin.py
@@ -3,4 +3,7 @@ from .models import Tour
 
 # Register your models here.
 @admin.register(Tour)
-
+class TourAdmin(admin.ModelAdmin):
+    list_display = ('origin_country', 'destination_country', 'number_of_nights', 'price')
+    search_fields = ('origin_country', 'destination_country')
+    list_filter = ('price', 'number_of_nights') 
