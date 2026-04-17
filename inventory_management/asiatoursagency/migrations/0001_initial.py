ecfe7bd2ae669fea6a19e52adbfeff94bd78c590
isaacandresbaleero-hub <isaacandresbaleero@gmail.com>
2026-04-17 13:46:09 +0300

    how to use python shell

--- inventory_management/asiatoursagency/migrations/0001_initial.py
+++ inventory_management/asiatoursagency/migrations/0001_initial.py
@@ -12,7 +12,7 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='tour',
+            name='Tour',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('origin_country', models.CharField(max_length=64)),
--- inventory_management/asiatoursagency/models.py
+++ inventory_management/asiatoursagency/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 
 # Create your models here.
-class tour(models.Model):
+class Tour(models.Model):
     # we need origin country, destinaiton,number of nights, and price.
     origin_country = models.CharField(max_length=64)
     destination_country = models.CharField(max_length=64)
     number_of_nights = models.IntegerField()
     price = models.IntegerField()
+
+#string representation of the tour
+    def __str__(self):
+        return (f"ID: {self.id}, Origin: {self.origin_country}, Destination: {self.destination_country}, Nights: {self.number_of_nights}, Price: {self.price}  ")
